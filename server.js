3a1564a675cb946248213c36b772236bb9aa4e02
Glitch (glitch-hello-node) <none>
2023-07-24 04:42:57 +0000

    🌅❣️ Checkpoint ./server.js:56651887/3421 ./package.json:56651887/986

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-// Import express module
 const express = require('express');
+const axios = require('axios');
 
-// Create a new express application
 const app = express();
-
-// Use express's JSON middleware to automatically parse JSON requests
 app.use(express.json());
 
-// Define the function to parse the payload
+// SendBird API Base URL
+const SEND_BIRD_API_BASE_URL = 'https://api-D70D1F08-9EEB-4C33-82B6-639E6D652564.sendbird.com/v3';
+
+// SendBird API token
+const SEND_BIRD_API_TOKEN = '8b42407cfe3aee731bb99d546148e8c6ff46173a';
+
+// Function to parse the payload
 function parsePayload(payload) {
-  // Parse the 'event' field into an actual JSON object
   let event = {};
   try {
     event = JSON.parse(payload.event.replace(/(\w+):/g, '"$1":'));
@@ -17,32 +19,45 @@ function parsePayload(payload) {
     console.error('Error parsing event:', e);
   }
 
-  // Construct an object from the parsed parts of the payload
-  const parsedData = {
+  return {
     ticket_id: payload.ticket_id,
     new_agent_email: payload.new_agent_email,
     event_responder_id_from: event.responder_id?.from,
     event_responder_id_to: event.responder_id?.to,
     sender_id: payload.sender_id
   };
-
-  return parsedData;
 }
 
-// Define an endpoint at /tickets for POST requests
-app.post('/tickets', (req, res) => {
-  // Get the payload from the request
-  const payload = req.body;
+// Helper function to remove a user from a SendBird channel
+async function removeFromChannel(channelUrl, userId) {
+  await axios.delete(`${SEND_BIRD_API_BASE_URL}/group_channels/${channelUrl}/leave/${userId}`, {
+    headers: { 'Api-Token': SEND_BIRD_API_TOKEN }
+  });
+}
 
-  // Parse the payload using the parsePayload function
-  const parsedData = parsePayload(payload);
+// Helper function to add a user to a SendBird channel
+async function addToChannel(channelUrl, userId) {
+  await axios.post(`${SEND_BIRD_API_BASE_URL}/group_channels/${channelUrl}/invite`, {
+    user_ids: [userId]
+  }, {
+    headers: { 'Api-Token': SEND_BIRD_API_TOKEN }
+  });
+}
 
-  // Log the parsed data as JSON
-  console.log(JSON.stringify(parsedData, null, 2));
-
-  // Send a response
-  res.send('Received and parsed your request!');
+app.post('/tickets', async (req, res) => {
+  try {
+    const parsedData = parsePayload(req.body);
+    const channelUrl = `freshdesk_${parsedData.ticket_id}`;
+  
+    await removeFromChannel(channelUrl, parsedData.event_responder_id_from.toString());
+    await addToChannel(channelUrl, parsedData.event_responder_id_to.toString());
+  
+    console.log(JSON.stringify(parsedData, null, 2));
+    res.send('Received and parsed your request!');
+  } catch (error) {
+    console.error('Error processing request:', error);
+    res.status(500).send('Error processing request.');
+  }
 });
 
-// Start the server on port 3000
 app.listen(3000, () => console.log('Server started on port 3000'));
