1289ef2ac4e8ceb1804129254c4a8a77af2a1aa0
Glitch (glitch-hello-node) <none>
2023-07-24 04:07:47 +0000

    👌🐆 Checkpoint ./server.js:56651887/8405 ./package.json:56651887/1399

--- server.js
+++ server.js
@@ -1,119 +1,48 @@
-/**
- * This is the main Node.js server script for your project
- * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
- */
+// Import express module
+const express = require('express');
 
-const path = require("path");
+// Create a new express application
+const app = express();
 
-// Require the fastify framework and instantiate it
-const fastify = require("fastify")({
-  // Set this to true for detailed logging:
-  logger: false,
-});
+// Use express's JSON middleware to automatically parse JSON requests
+app.use(express.json());
 
-// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+// Define the function to parse the payload
+function parsePayload(payload) {
+  // Parse the 'event' field into an actual JSON object
+  let event = {};
+  try {
+    event = JSON.parse(payload.event.replace(/(\w+):/g, '"$1":'));
+  } catch (e) {
+    console.error('Error parsing event:', e);
+  }
 
-// Setup our static files
-fastify.register(require("@fastify/static"), {
-  root: path.join(__dirname, "public"),
-  prefix: "/", // optional: default '/'
-});
+  // Construct an object from the parsed parts of the payload
+  const parsedData = {
+    ticket_id: payload.ticket_id,
+    new_agent_email: payload.new_agent_email,
+    event_responder_id_from: event.responder_id?.from,
+    event_responder_id_to: event.responder_id?.to,
+    sender_id: payload.sender_id
+  };
 
-// Formbody lets us parse incoming forms
-fastify.register(require("@fastify/formbody"));
-
-// View is a templating manager for fastify
-fastify.register(require("@fastify/view"), {
-  engine: {
-    handlebars: require("handlebars"),
-  },
-});
-
-// Load and parse SEO data
-const seo = require("./src/seo.json");
-if (seo.url === "glitch-default") {
-  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
+  return parsedData;
 }
 
-/**
- * Our home page route
- *
- * Returns src/pages/index.hbs with data built into it
- */
-fastify.get("/", function (request, reply) {
-  // params is an object we'll pass to our handlebars template
-  let params = { seo: seo };
+// Define an endpoint at /tickets for POST requests
+app.post('/tickets', (req, res) => {
+  // Get the payload from the request
+  const payload = req.body;
 
-  // If someone clicked the option for a random color it'll be passed in the querystring
-  if (request.query.randomize) {
-    // We need to load our color data file, pick one at random, and add it to the params
-    const colors = require("./src/colors.json");
-    const allColors = Object.keys(colors);
-    let currentColor = allColors[(allColors.length * Math.random()) << 0];
+  // Parse the payload using the parsePayload function
+  const parsedData = parsePayload(payload);
 
-    // Add the color properties to the params object
-    params = {
-      color: colors[currentColor],
-      colorError: null,
-      seo: seo,
-    };
-  }
+  // Log the parsed data as JSON
+  console.log(JSON.stringify(parsedData, null, 2));
 
-  // The Handlebars code will be able to access the parameter values and build them into the page
-  return reply.view("/src/pages/index.hbs", params);
+  // Send a response
+  res.send('Received and parsed your request!');
 });
 
-/**
- * Our POST route to handle and react to form submissions
- *
- * Accepts body data indicating the user choice
- */
-fastify.post("/", function (request, reply) {
-  // Build the params object to pass to the template
-  let params = { seo: seo };
-
-  // If the user submitted a color through the form it'll be passed here in the request body
-  let color = request.body.color;
-
-  // If it's not empty, let's try to find the color
-  if (color) {
-    // ADD CODE FROM TODO HERE TO SAVE SUBMITTED FAVORITES
-
-    // Load our color data file
-    const colors = require("./src/colors.json");
-
-    // Take our form submission, remove whitespace, and convert to lowercase
-    color = color.toLowerCase().replace(/\s/g, "");
-
-    // Now we see if that color is a key in our colors object
-    if (colors[color]) {
-      // Found one!
-      params = {
-        color: colors[color],
-        colorError: null,
-        seo: seo,
-      };
-    } else {
-      // No luck! Return the user value as the error property
-      params = {
-        colorError: request.body.color,
-        seo: seo,
-      };
-    }
-  }
-
-  // The Handlebars template will use the parameter values to update the page with the chosen color
-  return reply.view("/src/pages/index.hbs", params);
-});
-
-// Run the server and report out to the logs
-fastify.listen(
-  { port: process.env.PORT, host: "0.0.0.0" },
-  function (err, address) {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`Your app is listening on ${address}`);
-  }
-);
+// Start the server on port 3000
+app.listen(3000, () => console.log('Server started on port 3000'));
