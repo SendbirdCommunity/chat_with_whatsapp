3b7cf07bcf8b5c8a0fc083790df84c2f773158d1
Glitch (glitch-hello-node) <none>
2023-08-08 23:02:46 +0000

    👴🎲 Checkpoint ./server.js:56651887/107

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 
 // SendBird API Base URL
 const SEND_BIRD_API_BASE_URL = 'https://api-D70D1F08-9EEB-4C33-82B6-639E6D652564.sendbird.com/v3';
