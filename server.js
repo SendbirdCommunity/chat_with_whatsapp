96c0fc202b999a76bd52e999654c26f72ee1b913
Glitch (glitch-hello-node) <none>
2024-01-24 02:39:57 +0000

    🌞👼 Checkpoint ./server.js:56651887/62

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ app.get("/joke", async (req, res) => {
   console.log(req)
   const joke = `Why did the banana go to the doctor?
   Because it wasn't peeling well! 🍌😊`
-  res.status(200).send(joke);
+  res.status(200).send(JSON.stringify(joke));
 });
-
-
 
 
 app.listen(3000, () => console.log("Server started on port 3000"));
