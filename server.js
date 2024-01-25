89540e2d24c67352fa37f398d08a887d0502c5a7
Glitch (glitch-hello-node) <none>
2024-01-25 23:53:43 +0000

    🌷👝 Checkpoint ./server.js:56651887/2155

--- server.js
+++ server.js
@@ -4,48 +4,38 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-function createLead(content) {
+async function createLead(content) {
   
-  const { first_name, last_name, email, description   } = content
+  const { first_name, last_name, email, description, token } = content;
   
-  
-  let data = JSON.stringify({
-    data: {
-      first_name,
-      last_name,
-      email,
-      description: "The user looks like a typical lead for these reasons.",
-    },
-  });
+  let data = { 
+    data: { 
+      first_name: first_name || "Bob" , 
+      last_name: last_name || "Smith", 
+      email: email || "a@b.com", 
+      description: description || "SOME TEXT" 
+    }
+  }
+ console.log(data)
+  let config = { headers: { "Content-Type": "application/json", Authorization: token}};
 
-  let config = {
-    method: "post",
-    maxBodyLength: Infinity,
-    url: "https://api.getbase.com/v2/leads",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization:
-        "Bearer 810eb98743bd317d704e91135b6179ef00d6b0d17849d7f0b95e160388ca4657",
-    },
-    data: data,
-  };
-
-  axios
-    .request(config)
-    .then((response) => {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  try {
+    const response = await axios.post("https://api.getbase.com/v2/leads", data, config);
+    const result = JSON.stringify(response.data);
+    return result 
+  } catch (error) {
+    // console.error(error.request);
+    return JSON.stringify(error)
+  }
 }
+
 
 app.post("/lead", async (req, res) => {
   const token = req.headers.authorization || null;
-  console.log(token);
-  console.log(req.body);
+  const lead = await createLead({...req.body, token})
   //https://api.getbase.com/v2/leads
-  res.status(200).send("okay");
+  console.log(lead)
+  res.status(200).send(JSON.stringify(lead));
 });
 
 app.listen(3000, () => console.log("Server started on port 3000"));
