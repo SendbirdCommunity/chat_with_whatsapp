e2ff17151da4c81bb7946a5668fed441ae7d6b1f
Glitch (glitch-hello-node) <none>
2024-01-26 00:16:30 +0000

    🍨🌞 Checkpoint ./server.js:56651887/46

--- server.js
+++ server.js
@@ -4,15 +4,17 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
+
+
 async function createLead(content) {
   
   const { first_name, last_name, email, description, token } = content;
   
   let data = { 
     data: { 
-      first_name: first_name || "Bob" , 
-      last_name: last_name || "Smith", 
-      email: email || "a@b.com", 
+      first_name: first_name || "NOT GIVEN" , 
+      last_name: last_name || "NOT GIVEN", 
+      email: email || "NOT GIVEN", 
       description: description || "SOME TEXT" 
     }
   }
@@ -27,11 +29,13 @@ async function createLead(content) {
   }
 }
 
+
 app.post("/lead", async (req, res) => {
   const token = req.headers.authorization || null;
   const lead = await createLead({...req.body, token})
   console.log(lead)
   res.status(200).send(lead);
 });
+
 
 app.listen(3000, () => console.log("Server started on port 3000"));
