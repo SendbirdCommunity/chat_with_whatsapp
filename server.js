65f4cb569efe79faf6d0febde7c4228d0daad0d6
Glitch (glitch-hello-node) <none>
2024-01-29 23:05:07 +0000

    💓🎍 Checkpoint ./server.js:56651887/950

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
 const express = require("express");
 const axios = require("axios");
+const bodyParser = require('body-parser');
 const crypto = require("crypto");
 
 // Initialize Express application
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware to access the raw body
+app.use(express.json({verify: (req, _, buf) => {req.rawBody = buf;}}));
+app.use(express.urlencoded({extended: true,verify: (req, _, buf) => {req.rawBody = buf}}));
 
 
 const verifySlackRequest = (req, res, next) => {
   
   
-    const slackSignature = req.headers["x-slack-signature"];
-    const requestTimestamp = Number(req.headers['x-slack-request-timestamp']);
-    console.log(slackSignature)
-    console.log(requestTimestamp)
+//     const slackSignature = req.headers["x-slack-signature"];
+//     const requestTimestamp = Number(req.headers['x-slack-request-timestamp']);
+//     console.log(slackSignature)
+//     console.log(requestTimestamp)
 
-  // console.log(req.rawBody); // Make sure req.rawBody is populated as expected
+//   // console.log(req.rawBody); // Make sure req.rawBody is populated as expected
 
-  const sigBasestring = `v0:${requestTimestamp}:${req.body}`;
-  console.log(sigBasestring)
-  const mySignature = "v0=" + crypto.createHmac("sha256", process.env.SLACK_SIGNING_SECRET)
-                                   .update(sigBasestring, "utf8")
-                                   .digest("hex");
+//   const sigBasestring = `v0:${requestTimestamp}:${req.body}`;
+//   console.log(sigBasestring)
+//   const mySignature = "v0=" + crypto.createHmac("sha256", process.env.SLACK_SIGNING_SECRET)
+//                                    .update(sigBasestring, "utf8")
+//                                    .digest("hex");
+  
+//   console.log(mySignature)
 
 
 
@@ -32,10 +34,12 @@ const verifySlackRequest = (req, res, next) => {
 
 
 // Define POST endpoint for creating a lead
-app.post("/message_to_bot", express.text({type: ''}), async (req, res) => {
+app.post("/message_to_bot",  async (req, res) => {
 
   // Proceed with processing the request
   // Your logic here
+  
+  console.log({rawBody: req.rawBody});
 
   res.status(200).send("Request verified");
 });
