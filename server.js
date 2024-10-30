55fdd7c3e59208ba35ac878558cbd894608eba3f
Glitch (glitch-hello-node) <none>
2024-10-30 09:41:32 +0000

    👆📙 Checkpoint ./server.js:56651887/1035

--- server.js
+++ server.js
@@ -13,9 +13,30 @@ app.use(express.urlencoded({ extended: true, verify: (req, _, buf) => { req.rawB
 
 
 app.post("/messages",  async (req, res) => {
+    console.log("POSTED HERE")
     console.log(req.body)
+    const changes = req.body.entry
+    changes.forEach(entry => {
+      console.log(entry.changes)
+    })
     res.sendStatus(200); // Acknowledge the event immediately
 
+});
+
+const VERIFY_TOKEN = "mySecureVerifyToken123!";  // Replace with your custom token
+
+app.get("/messages", (req, res) => {
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verify_token"];
+    const challenge = req.query["hub.challenge"];
+
+    if (mode && token === VERIFY_TOKEN) {
+        // Token verified successfully, return challenge back to WhatsApp
+        res.status(200).send(challenge);
+    } else {
+        // Token verification failed
+        res.sendStatus(403);
+    }
 });
 
 /**
